e0c7fdb09ad74fe5e3d66e1ba45b09e459b974fa
gyw33188 <gyw33188@ly.com>
2019-10-11 21:04:45 +0800

    first commit

---
+++ src/main/java/site/guyw/grpg/WeiXinToken.java
@@ -0,0 +1,126 @@
+package site.guyw.grpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author conangu(顾永威)
+ * @createTime 2019-10-11 15:22
+ * @description 微信token验证
+ */
+@Controller
+@EnableAutoConfiguration
+
+public class WeiXinToken {
+    private static String token     = "siteguywtoken";
+    private static String appId     = "wx322a6bd51d1db0f5";
+    private static String appSecret = "f900a01969566f1e219d01445a3913cc";
+
+
+    //设置访问的url
+    @RequestMapping(value = "/token", method = RequestMethod.GET)
+    //表示返回JSON格式的结果，如果前面使用的是@RestController可以不用写
+    @ResponseBody
+    public String tokenVerify(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce,
+                              @RequestParam("echostr") String echostr) {
+
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (signature != null && checkSignature(signature, timestamp, nonce)) {
+            return echostr;
+        }
+
+        return "hello";
+    }
+
+    public static void main(String[] args) throws Exception {
+        //通过SpringApplication的run()方法启动应用，无需额外的配置其他的文件
+        SpringApplication.run(WeiXinToken.class, args);
+    }
+
+    /**
+     * 验证签名
+     *
+     * @param signature
+     * @param timestamp
+     * @param nonce
+     * @return
+     */
+    public static boolean checkSignature(String signature, String timestamp, String nonce) {
+        String[] arr = new String[] { token, timestamp, nonce };
+        // 将token、timestamp、nonce三个参数进行字典序排序
+        // Arrays.sort(arr);
+        sort(arr);
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            content.append(arr[i]);
+        }
+        MessageDigest md = null;
+        String tmpStr = null;
+
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+            // 将三个参数字符串拼接成一个字符串进行sha1加密
+            byte[] digest = md.digest(content.toString().getBytes());
+            tmpStr = byteToStr(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        content = null;
+        // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
+        return tmpStr != null ? tmpStr.equals(signature.toUpperCase()) : false;
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param byteArray
+     * @return
+     */
+    private static String byteToStr(byte[] byteArray) {
+        String strDigest = "";
+        for (int i = 0; i < byteArray.length; i++) {
+            strDigest += byteToHexStr(byteArray[i]);
+        }
+        return strDigest;
+    }
+
+    /**
+     * 将字节转换为十六进制字符串
+     *
+     * @param mByte
+     * @return
+     */
+    private static String byteToHexStr(byte mByte) {
+        char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        char[] tempArr = new char[2];
+        tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+        tempArr[1] = Digit[mByte & 0X0F];
+        String s = new String(tempArr);
+        return s;
+    }
+
+    public static void sort(String a[]) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j].compareTo(a[i]) < 0) {
+                    String temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main/java/site/guyw/grpg/service/AccessTokenService.java
@@ -0,0 +1,38 @@
+package site.guyw.grpg.service;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+import site.guyw.grpg.HttpUtils;
+import site.guyw.grpg.common.AccessTokenResp;
+
+/**
+ * @author conangu(顾永威)
+ * @createTime 2019-10-11 20:30
+ * @description accessToken
+ */
+@Service
+public class AccessTokenService {
+    private String accessToken;
+    private static String accessTokenUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx322a6bd51d1db0f5&secret=f900a01969566f1e219d01445a3913cc";
+
+    private long time = 0;
+    public String getAccessToken(){
+
+        if (time <= System.currentTimeMillis()){
+            accessToken = null;
+        }
+        if (StringUtils.isBlank(accessToken)){
+            AccessTokenResp accessTokenResp;
+            String res = HttpUtils.get(accessTokenUrl);
+            System.out.println(res);
+            accessTokenResp  = JSON.parseObject(res,AccessTokenResp.class);
+            accessToken = accessTokenResp.getAccess_token();
+            if (StringUtils.isNotBlank(accessToken)){
+                time = System.currentTimeMillis() + accessTokenResp.getExpires_in()*1000;
+            }
+        }
+        return accessToken;
+    }
+
+}
